dbb770209fd51a14b55bb1af627e7bc4c0dfe3fe
nikospara <nikospara@yahoo.com>
2013-06-16 20:29:02 +0300

    Updated require.js and r.js to 2.1.6 and text to 2.0.7

--- examples/shim/WebContent/scripts/require-cfg.js
+++ examples/shim/WebContent/scripts/require-cfg.js
@@ -2,7 +2,7 @@ require.config({
 	baseUrl: "scripts",
 	
 	paths: {
-		"text": "lib/text-2.0.5",
+		"text": "lib/text-2.0.7",
 		"lazy": "lib/lazy",
 		"promise-adaptor": "lib/promise-adaptor-jquery"
 	},

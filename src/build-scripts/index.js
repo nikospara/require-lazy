a99fc507324db84f09e16fec9c72d44f3dc478ef
nikospara <nikospara@yahoo.com>
2013-10-24 22:31:39 +0300

    Initial work for integrating with npm

---
+++ src/build-scripts/index.js
@@ -0,0 +1,8 @@
+var
+	findDeps = require("./find-deps"),
+	processModules = require("./process-modules"),
+	buildAll = require("./build-all");
+
+module.exports.findDeps = findDeps;
+module.exports.processModules = processModules;
+module.exports.buildAll = buildAll;
